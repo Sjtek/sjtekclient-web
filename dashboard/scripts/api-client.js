7a9d0f32b827b000cd5be78d14e96beb859013ad
Tijn Renders <tijnrenders@gmail.com>
2016-08-25 13:22:45 +0200

    Temporary commit for migration to other PC

---
+++ dashboard/scripts/api-client.js
@@ -0,0 +1,51 @@
+function send(command) {
+    var url = "https://sjtek.nl/api/" + command;
+    $.get(url, function(data) {
+        var obj = JSON.parse(data);
+        $('#tempIn').text(obj.temperature.inside);
+        $('#tempOut').text(obj.temperature.outside);
+        $('#musicStatus').text(obj.music.state);
+        $('#musicTitle').text(obj.music.song.title);
+        $('#musicArtist').text(obj.music.song.artist);
+        $('#quote').text(obj.quotes.quote);
+        parseSonarr(obj);
+    });
+}
+
+function parseSonarr(data) {
+    var episode = data.sonarr.upcoming[0];
+    var text = episode.seriesTitle + " S" + episode.seasonInt + "E" + episode.episodeInt + "<br>";
+    text += episode.episodeName + "<br>";
+    text += episode.airDateUTC;
+    document.querySelector("#sonarr").innerHTML = text;
+}
+
+/**
+ * @function httpRequest - Wrapper for JS XMLHttpRequest
+ *
+ * @param {string} type - Type of request as a string. Can be either "POST" or "GET"
+ * @param {string} url - URL for the request
+ * @param {function} callback - Callback function to process data
+ *
+ * @return {void}
+ *
+ * @example This will print the data from the example json in the console
+ * httpRequest(
+    "GET", //Type of request
+    "http://myjsonlocation.com/example.json", //Request url
+    function(data){
+      console.log(data.responseText);
+    });
+ *
+ */
+function httpRequest(type, url, callback){
+  var request;
+  request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      callback(request);
+    }
+  };
+  request.open(type, url, true);
+  request.send();
+}
