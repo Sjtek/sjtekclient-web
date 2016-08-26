12365c784b711a355ad6260bac4a686e8effe950
Tijn Renders <tijnrenders@gmail.com>
2016-08-26 12:05:41 +0200

    ES6ified code

--- dashboard/scripts/api-client.js
+++ dashboard/scripts/api-client.js
@@ -1,14 +1,21 @@
+//Enable strict mode for ES6 support
+'use strict';
+
+const baseUrl = 'https://sjtek.nl/api/';
+
 function send(command) {
-    var url = "https://sjtek.nl/api/" + command;
-    $.get(url, function(data) {
-        var obj = JSON.parse(data);
-        $('#tempIn').text(obj.temperature.inside);
-        $('#tempOut').text(obj.temperature.outside);
-        $('#musicStatus').text(obj.music.state);
-        $('#musicTitle').text(obj.music.song.title);
-        $('#musicArtist').text(obj.music.song.artist);
-        $('#quote').text(obj.quotes.quote);
-        parseSonarr(obj);
+    let url = baseUrl + command;
+
+    httpRequest("GET", url, function(data) {
+      console.warn(data.responseText);
+        // var obj = JSON.parse(data);
+        // $('#tempIn').text(obj.temperature.inside);
+        // $('#tempOut').text(obj.temperature.outside);
+        // $('#musicStatus').text(obj.music.state);
+        // $('#musicTitle').text(obj.music.song.title);
+        // $('#musicArtist').text(obj.music.song.artist);
+        // $('#quote').text(obj.quotes.quote);
+        // parseSonarr(obj);
     });
 }
 
@@ -19,6 +26,8 @@ function parseSonarr(data) {
     text += episode.airDateUTC;
     document.querySelector("#sonarr").innerHTML = text;
 }
+
+
 
 /**
  * @function httpRequest - Wrapper for JS XMLHttpRequest
@@ -38,14 +47,14 @@ function parseSonarr(data) {
     });
  *
  */
-function httpRequest(type, url, callback){
-  var request;
-  request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
-      callback(request);
-    }
-  };
-  request.open(type, url, true);
-  request.send();
+function httpRequest(type, url, callback) {
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            callback(request);
+        }
+    };
+    request.open(type, url, true);
+    request.send();
 }
